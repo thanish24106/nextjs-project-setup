3b0cf3700535afc45f13d82d619af5f6a2892f34
thanish24106 <154069264+thanish24106@users.noreply.github.com>
2025-05-20 02:21:39 +0530

    Add .next/static/chunks/src_app_manager_dashboard_page_tsx_2facd0d2._.js 1747687898091

---
+++ .next/static/chunks/src_app_manager_dashboard_page_tsx_2facd0d2._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_bd4a6d59._.js",
+  "static/chunks/src_29a60be9._.js"
+],
+    source: "dynamic"
+});
