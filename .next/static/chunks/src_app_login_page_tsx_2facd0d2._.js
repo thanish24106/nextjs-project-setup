d2751f0749932220da191b50c20fab197ed3527f
thanish24106 <154069264+thanish24106@users.noreply.github.com>
2025-05-20 02:21:47 +0530

    Add .next/static/chunks/src_app_login_page_tsx_2facd0d2._.js 1747687906091

---
+++ .next/static/chunks/src_app_login_page_tsx_2facd0d2._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_bcde8748._.js",
+  "static/chunks/src_c3e0f819._.js"
+],
+    source: "dynamic"
+});
